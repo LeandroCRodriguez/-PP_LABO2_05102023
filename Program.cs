68b217a2c49009b0a1cb5188ef071f49fb74f1c5
LeandroCRodriguez <leandroguit87@gmail.com>
2023-10-04 17:55:03 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParcialLabo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
